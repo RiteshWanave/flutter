5d39f4090f6ec8d73fc52e17bc119b28e8d66430
Ritesh <ritesh@gmail.com>
2021-10-04 09:04:40 +0530

    second

---
+++ asgn1.c
@@ -0,0 +1,35 @@
+// Q1 Write C program to Increment & Decrement operations using Pointer
+
+#include <stdio.h>
+
+int main(){
+
+    int a , b;
+    int *ptra , *ptrb;
+
+    scanf("%d %d" , &a , &b);
+
+    ptra = &a;
+    ptrb = &b;
+
+    ++*ptra;
+    --*ptrb;
+
+    printf("%d and %d" , *ptra, *ptrb);
+
+    return 0;
+}
+
+
+
+// Q2 Write a C program to find the length of the string using pointer
+
+#include <stdio.h>
+
+int main(){
+
+    
+
+    return 0;
+
+}
